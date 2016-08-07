f24faba7fa3cdcbecd9af157cc85f6b8c6212eb8
KidSea <594643219@qq.com>
2016-08-07 16:56:01 +0800

    Commit 3

--- src/com/example/mobilesavety/activity/SplashActivity.java
+++ src/com/example/mobilesavety/activity/SplashActivity.java
@@ -1,23 +1,44 @@
 package com.example.mobilesavety.activity;
 
-
 import com.example.mobilesavety.R;
+import com.example.mobilesavety.utils.PackageUtils;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
+
 /**
  * Splash界面
+ * 
  * @author yuxuehai
- *
+ * 
  */
 public class SplashActivity extends Activity {
-	
+
+	private TextView mTvVersion;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
+
+		initDate();
+		initView();
 	}
+
+	private void initView() {
+
+		mTvVersion = (TextView) findViewById(R.id.textView);
+		//获取包的管理z
+		mTvVersion.setText("版本号：" + PackageUtils.getVersionCode(this));
+
+	}
+
+	private void initDate() {
+
+	}
+
 }
