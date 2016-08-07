e9a8c159e19f6ee0934d8d6f595c9c912738c2ee
KidSea <594643219@qq.com>
2016-08-07 16:17:39 +0800

    New app

---
+++ src/com/example/mobilesavety/activity/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.mobilesavety.activity;
+
+import com.example.mobilesavety.R;
+import com.example.mobilesavety.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	
+}
---
+++ src/com/example/mobilesavety/activity/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobilesavety.activity;
+
+
+import com.example.mobilesavety.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_splash);
+	}
+}
